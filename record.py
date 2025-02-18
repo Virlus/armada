fb02477b86416bea35c8a02bb7b4d245ae0be7a4
Virlus <maomao4171@163.com>
2025-02-18 15:01:04 +0800

    minished image resolution and adopted relative ee pose as action repr

--- record.py
+++ record.py
@@ -6,6 +6,9 @@ from scipy.spatial.transform import Rotation as R
 import cv2
 from typing import List
 import argparse
+import torch
+from torchvision.transforms import Compose, Resize
+from torchvision.transforms import InterpolationMode
 
 from hardware.my_device.camera import CameraD400
 from hardware.my_device.robot import FlexivRobot, FlexivGripper
@@ -16,22 +19,14 @@ from third_party.diffusion_policy.diffusion_policy.common.replay_buffer import R
 
 camera_serial = ["038522063145", "104422070044"]
 
-def save_data(
-        color_image, 
-        depth_image, 
-        color_dir,
-        depth_dir,):
-    color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-    Image.fromarray(color_image).save(color_dir)
-    Image.fromarray(depth_image).save(depth_dir)
 
 def record(replay_buffer:ReplayBuffer, robot:FlexivRobot, gripper:FlexivGripper, cameras:List[CameraD400], sigma:Sigma7, \
-           keyboard: Keyboard):
+           keyboard: Keyboard, image_processor: Compose):
     start_time = int(time.time() * 1000) # doesn't matter
     
     # color_image, depth_image = camera.get_data()
-    # color_image: 460,640,3  0~255
-    # depth_image: 460,640  0~4681 [mm]
+    # color_image: 480,640,3  0~255
+    # depth_image: 480,640  0~4681 [mm]
 
     tcp_pose = []
     joint_pos = []
@@ -44,6 +39,10 @@ def record(replay_buffer:ReplayBuffer, robot:FlexivRobot, gripper:FlexivGripper,
     keyboard.finish = False
     cnt = 0
     start_time = time.time()
+    
+    # Relative ee pose as action space
+    last_p, last_r, width = sigma.get_control()
+
     while not keyboard.quit and not keyboard.discard and not keyboard.finish:
         time.sleep(max(0.1 - (time.time() - start_time), 0))
         start_time = time.time()
@@ -54,22 +53,28 @@ def record(replay_buffer:ReplayBuffer, robot:FlexivRobot, gripper:FlexivGripper,
         tcpPose, jointPose, _, _ = robot.get_robot_state()
         
         diff_p, diff_r, width = sigma.get_control()
-        diff_p = diff_p + robot.init_pose[:3]
-        diff_r = R.from_quat(robot.init_pose[3:]) * diff_r
+        curr_p_action = diff_p - last_p
+        curr_r_action = last_r.inv() * diff_r
+        last_p = diff_p
+        last_r = diff_r
+        curr_p = diff_p + robot.init_pose[:3]
+        curr_r = R.from_quat(robot.init_pose[3:]) * diff_r
         # Send command.
-        robot.send_tcp_pose(np.concatenate((diff_p,diff_r.as_quat()), 0))
+        robot.send_tcp_pose(np.concatenate((curr_p,curr_r.as_quat()), 0))
         gripper.move_from_sigma(width)
         gripper_width = gripper.max_width * width / 1000
         if not keyboard.start:
             continue
         cnt += 1
-        wrist_image = cv2.cvtColor(cam_data[1][0].copy(), cv2.COLOR_BGR2RGB)
-        side_image = cv2.cvtColor(cam_data[0][0].copy(), cv2.COLOR_BGR2RGB)
+        wrist_image = image_processor(torch.from_numpy(cv2.cvtColor(cam_data[1][0].copy(), cv2.COLOR_BGR2RGB)).\
+                                      permute(2,0,1)).permute(1,2,0).detach().cpu().numpy().astype(np.uint8)
+        side_image = image_processor(torch.from_numpy(cv2.cvtColor(cam_data[0][0].copy(), cv2.COLOR_BGR2RGB)).\
+                                      permute(2,0,1)).permute(1,2,0).detach().cpu().numpy().astype(np.uint8)
         wrist_cam.append(wrist_image)
         side_cam.append(side_image)
         tcp_pose.append(tcpPose)
         joint_pos.append(jointPose)
-        action.append(np.concatenate((diff_p,diff_r.as_quat(), [gripper_width])))
+        action.append(np.concatenate((curr_p_action, curr_r_action.as_quat(), [gripper_width])))
 
     if not keyboard.start or keyboard.quit or keyboard.discard:
         print('WARNING: discard the demo!')
@@ -95,15 +100,20 @@ def main(args):
     keyboard = Keyboard()
     zarr_path = os.path.join(args.output, 'replay_buffer.zarr')
     replay_buffer = ReplayBuffer.create_from_path(zarr_path, mode='a')
+    # Image processing
+    img_res = args.resolution
+    BICUBIC = InterpolationMode.BICUBIC
+    image_processor = Compose([Resize(img_res, interpolation=BICUBIC)])
     while not keyboard.quit:
         print("start recording...")
-        record(replay_buffer, robot, gripper, camera, sigma, keyboard)
+        record(replay_buffer, robot, gripper, camera, sigma, keyboard, image_processor)
         if not keyboard.quit:
             print("reset the environment...")
             time.sleep(10)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', type=str, default='/mnt/workspace/DP/0217_stack_cups')
+    parser.add_argument('-o', '--output', type=str, default='/mnt/workspace/DP/0218_stack_cups')
+    parser.add_argument('-res', '--resolution', nargs='+', type=int)
     args = parser.parse_args()
     main(args)
--- replay_demo.py
+++ replay_demo.py
@@ -1,6 +1,8 @@
 from hardware.my_device.robot import FlexivRobot, FlexivGripper
 from third_party.diffusion_policy.diffusion_policy.common.replay_buffer import ReplayBuffer
+from scipy.spatial.transform import Rotation as R
 import argparse
+import numpy as np
 import time
 
 def main(args):
@@ -11,15 +13,22 @@ def main(args):
     for i in range(replay_buffer.n_episodes):
         robot.send_tcp_pose(robot.init_pose)
         gripper.move(gripper.max_width)
+        last_p = robot.init_pose[:3]
+        last_r = R.from_quat(robot.init_pose[3:])
         time.sleep(2)
         for j in range(0 if i == 0 else replay_buffer.episode_ends[i-1], replay_buffer.episode_ends[i]):
             start_time = time.time()
-            robot.send_tcp_pose(replay_buffer['action'][j, :7])
+            curr_p = last_p + replay_buffer['action'][j, :3]
+            curr_r = last_r * R.from_quat(replay_buffer['action'][j, 3:7])
+            robot.send_tcp_pose(np.concatenate((curr_p, curr_r.as_quat()), 0))
+            last_p = curr_p
+            last_r = curr_r
+            # robot.send_tcp_pose(replay_buffer['action'][j, :7])
             gripper.move(replay_buffer['action'][j, 7])
             time.sleep(max(1 / 10 - (time.time() - start_time), 0))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--demo_path', type=str, default='/mnt/workspace/DP/0217_stack_cups/replay_buffer.zarr')
+    parser.add_argument('-p', '--demo_path', type=str, default='/mnt/workspace/DP/0218_stack_cups/replay_buffer.zarr')
     args = parser.parse_args()
     main(args)

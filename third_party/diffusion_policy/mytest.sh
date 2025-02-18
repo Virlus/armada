d2df2f3a531a79c94cfebce74518821f177f954f
Virlus <maomao4171@163.com>
2025-02-18 15:18:27 +0800

    training config renewed

--- third_party/diffusion_policy/mytest.sh
+++ third_party/diffusion_policy/mytest.sh
@@ -3,5 +3,6 @@ source "/home/yuwenye/miniconda3/etc/profile.d/conda.sh"; conda activate human_d
 
 export WANDB_API_KEY=956bd553be1193c6fdfce758ad1eded5190eac56
 export HYDRA_FULL_ERROR=1
+export CUDA_VISIBLE_DEVICES=3
 
 python train.py --config-name=train_diffusion_transformer_real_hybrid_workspace task.dataset_path=/mnt/workspace/DP/0217_PnP

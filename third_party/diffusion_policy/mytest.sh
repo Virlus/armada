6f0da9542ab752c0e4341b24c2c120e9dae48011
Virlus <maomao4171@163.com>
2025-02-19 17:02:18 +0800

    synced with training server

--- third_party/diffusion_policy/mytest.sh
+++ third_party/diffusion_policy/mytest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-source "/home/yuwenye/miniconda3/etc/profile.d/conda.sh"; conda activate human_dp
-
+source "/home/jinyang/miniconda3/etc/profile.d/conda.sh"; conda activate dp
 export WANDB_API_KEY=956bd553be1193c6fdfce758ad1eded5190eac56
 export HYDRA_FULL_ERROR=1
 export CUDA_VISIBLE_DEVICES=3
 
-python train.py --config-name=train_diffusion_transformer_real_hybrid_workspace task.dataset_path=/mnt/workspace/DP/0217_PnP
+python train.py --config-name=train_diffusion_transformer_real_hybrid_workspace task.dataset_path=/home/jinyang/yuwenye/human-in-the-loop/data/0219_PnP_fixed_init

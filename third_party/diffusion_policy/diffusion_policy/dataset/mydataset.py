b2578c4bd2535b4585f0434dd88dadc988f28603
Virlus <maomao4171@163.com>
2025-02-25 22:34:05 +0800

    fixed fatal dataset issue for rel ee pose

--- third_party/diffusion_policy/diffusion_policy/dataset/mydataset.py
+++ third_party/diffusion_policy/diffusion_policy/dataset/mydataset.py
@@ -89,7 +89,7 @@ class MyDataset(BaseImageDataset):
         side_img = np.moveaxis(sample['side_cam'],-1,1)/255
 
         if self.rel_ee_pose:
-            action_sample = sample['action']
+            action_sample = sample['action'].copy()
             base_pose = action_sample[self.n_obs_steps-2, :7]
             base_rot = R.from_quat(base_pose[3:][[1,2,3,0]])
             base_translate = base_pose[:3]
@@ -99,7 +99,7 @@ class MyDataset(BaseImageDataset):
                 action_sample[i, :3] = (curr_translate - base_translate) @ base_rot.as_matrix()
                 action_sample[i, 3:7] = (base_rot.inv() * curr_rot).as_quat()[[3,0,1,2]]
         else:
-            action_sample = sample['action']
+            action_sample = sample['action'].copy()
 
         data = {
             'obs': {
@@ -120,7 +120,7 @@ class MyDataset(BaseImageDataset):
 
 def test():
     import os
-    zarr_path = os.path.expanduser('/mnt/workspace/DP/0225_abs_PnP/replay_buffer.zarr')
+    zarr_path = os.path.expanduser('/home/jinyang/yuwenye/human-in-the-loop/data/0225_abs_PnP/replay_buffer.zarr')
     dataset = MyDataset(zarr_path, horizon=16, rel_ee_pose=True, n_obs_steps=2)
     import pdb; pdb.set_trace()
 

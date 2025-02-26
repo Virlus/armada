7a99a9a24eac0a38bb03e2e159e07032f775b4cb
Virlus <maomao4171@163.com>
2025-02-26 12:08:34 +0800

    replay renewed

--- replay_demo.py
+++ replay_demo.py
@@ -10,20 +10,15 @@ def main(args):
     gripper = FlexivGripper(robot)
     replay_buffer = ReplayBuffer.copy_from_path(args.demo_path, keys=['wrist_cam', 'side_cam', 'joint_pos', 'action'])
 
-    for i in range(replay_buffer.n_episodes-1, replay_buffer.n_episodes):
+    for i in range(replay_buffer.n_episodes):
         robot.send_tcp_pose(robot.init_pose)
         gripper.move(gripper.max_width)
-        last_p = robot.init_pose[:3]
-        last_r = R.from_quat(robot.init_pose[3:])
         time.sleep(2)
         for j in range(0 if i == 0 else replay_buffer.episode_ends[i-1], replay_buffer.episode_ends[i]):
             start_time = time.time()
-            curr_p = last_p + replay_buffer['action'][j, :3]
-            curr_r = last_r * R.from_quat(replay_buffer['action'][j, 3:7])
-            robot.send_tcp_pose(np.concatenate((curr_p, curr_r.as_quat()), 0))
-            last_p = curr_p
-            last_r = curr_r
-            # robot.send_tcp_pose(replay_buffer['action'][j, :7])
+            curr_p = replay_buffer['action'][j, :3]
+            curr_r = R.from_quat(replay_buffer['action'][j, [4,5,6,3]])
+            robot.send_tcp_pose(np.concatenate((curr_p, curr_r.as_quat()[[3,0,1,2]]), 0))
             if replay_buffer['action'][j, 7] == 0:
                 gripper.move(gripper.max_width)
             else:
@@ -32,6 +27,6 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--demo_path', type=str, default='/mnt/workspace/DP/0221_test/replay_buffer.zarr')
+    parser.add_argument('-p', '--demo_path', type=str, default='/mnt/workspace/DP/0225_abs_PnP/replay_buffer.zarr')
     args = parser.parse_args()
     main(args)

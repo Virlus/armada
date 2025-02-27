11697a1fdfc2acf2ce7b5372b09f43ed56309c2d
Virlus <maomao4171@163.com>
2025-02-27 12:09:39 +0800

    replay with image observations

--- replay_demo.py
+++ replay_demo.py
@@ -1,14 +1,19 @@
 from hardware.my_device.robot import FlexivRobot, FlexivGripper
 from third_party.diffusion_policy.diffusion_policy.common.replay_buffer import ReplayBuffer
+
 from scipy.spatial.transform import Rotation as R
+from PIL import Image
 import argparse
 import numpy as np
 import time
+import os
 
 def main(args):
     robot = FlexivRobot()
     gripper = FlexivGripper(robot)
     replay_buffer = ReplayBuffer.copy_from_path(args.demo_path, keys=['wrist_cam', 'side_cam', 'joint_pos', 'action'])
+    image_save_path = os.path.join(os.path.dirname(args.demo_path), 'images')
+    os.makedirs(image_save_path, exist_ok=True)
 
     for i in range(replay_buffer.n_episodes):
         robot.send_tcp_pose(robot.init_pose)
@@ -24,6 +29,12 @@ def main(args):
             else:
                 gripper.move(0)
             time.sleep(max(1 / 10 - (time.time() - start_time), 0))
+
+        for j in range(0 if i == 0 else replay_buffer.episode_ends[i-1], replay_buffer.episode_ends[i]):
+            episode_path = os.path.join(image_save_path, f'episode_{i}')
+            os.makedirs(episode_path, exist_ok=True)
+            side_img = replay_buffer['side_cam'][j]
+            Image.fromarray(side_img).save(os.path.join(episode_path, f'side_img_{j}.png'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

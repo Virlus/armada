8ac1e600b2b388f1c4db46de815219f7f06129c5
Wenye Yu <100664702+Virlus@users.noreply.github.com>
2025-04-28 14:16:35 +0800

    Delete third_party/diffusion_policy/mytest.sh

--- third_party/diffusion_policy/mytest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source "/data/yuwenye/miniconda3/etc/profile.d/conda.sh"; conda activate human_dp
-export WANDB_API_KEY=956bd553be1193c6fdfce758ad1eded5190eac56
-export HYDRA_FULL_ERROR=1
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-
-# python train.py --config-name=train_diffusion_transformer_hybrid_multi_gpu_workspace task.dataset_path=/home/jinyang/yuwenye/human-in-the-loop/data/0219_PnP_fixed_init
-# OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 python train.py train_diffusion_transformer_real_hybrid_multi_gpu_workspace
-OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 python train.py train_diffusion_transformer_real_hybrid_weighted_multi_gpu_workspace

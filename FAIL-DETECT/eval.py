54b60b4cf73ca53a0737e13649f6592ed8429bdb
Virlus <maomao4171@163.com>
2025-06-19 17:15:53 +0800

    [TODO] implement logpZO failure detection on real robot

--- FAIL-DETECT/eval.py
+++ FAIL-DETECT/eval.py
@@ -135,7 +135,7 @@ def main(rank, cfg, device_ids):
 
     predictor = FunctionalPredictor(modulation_type=ModulationType.Tfunc, regression_type=RegressionType.Mean)
     target_traj = predictor.get_one_sided_prediction_band(logpZO_list[:n_train], logpZO_list[-n_train:], alpha=0.05, lower_bound=False).flatten()
-    print(target_traj)
+    np.savez(f"/home/yuwenye/project/human-in-the-loop/baseline_data/outputs/{exp_name}/target_traj.npz", target_traj=target_traj)
     torch.distributed.destroy_process_group()
 
 

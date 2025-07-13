6105d1f901e29b14c752d56a480d1e6ceae7ca46
Virlus <maomao4171@163.com>
2025-07-14 01:17:07 +0800

    fixed replay buffer init

--- diffusion_policy/diffusion_policy/env_runner/real_robot_runner.py
+++ diffusion_policy/diffusion_policy/env_runner/real_robot_runner.py
@@ -183,12 +183,10 @@ class RealRobotRunner:
         zarr_path = os.path.join(self.eval_cfg.train_dataset_path, 'replay_buffer.zarr')
         
         # Determine which keys to load
-        dataset_keys = ['wrist_cam', 'side_cam', 'joint_pos', 'action', 'tcp_pose']
-        if 'round' in self.eval_cfg.train_dataset_path:
-            dataset_keys.append('action_mode')
-            self.replay_buffer = ReplayBuffer.copy_from_path(zarr_path, keys=dataset_keys)
-        else:
-            self.replay_buffer = ReplayBuffer.copy_from_path(zarr_path, keys=dataset_keys)
+        self.replay_buffer = ReplayBuffer.copy_from_path(zarr_path, keys=None)
+
+        # Add action_mode if not present
+        if 'action_mode' not in self.replay_buffer.keys():
             self.replay_buffer.data['action_mode'] = np.full((self.replay_buffer.n_steps, ), HUMAN)
         
         # Add failure_indices if not present
